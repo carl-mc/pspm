da924d660d83738187b9ff4e9a446e843d37af38
carl-mc <carl.schweinitz@gmail.com>
2025-10-06 12:12:27 +0200

    dismemberment and suffocation

--- inst/python/sl3.py
+++ inst/python/sl3.py
@@ -503,6 +503,183 @@ class VertexPackNS(NetworkStatistic):
         return deltas
 
 
+## Cracking
+class VertexDismembermentNS(NetworkStatistic):
+    # NS that evaluates how dismembered groups are
+    groups: List
+    weights: List
+    stat: List
+    
+    def __init__(self, spatial_lattice, partitioning, contributes_energy=True):
+        super().__init__(spatial_lattice, partitioning, contributes_energy)
+
+    def get_name(self) -> str:
+        return 'vertex_dismemberment'
+      
+    def get_statistic_shape(self) -> Tuple[int, int]:
+        return self.spatial_lattice.P, 1
+
+    def compute_statistic(self) -> None:
+        self.statistic: np.ndarray = np.zeros((1, self.spatial_lattice.P))
+        self.groups: List(np.array) = []
+        self.weights: List(np.array) = []
+        self.stat: List(np.array) = []
+        for c in range(self.spatial_lattice.P):
+          g = np.array(self.spatial_lattice.X[:,c])
+          gu, cnt = np.unique(g, return_counts=True)
+          self.groups.append(gu.astype(int))
+          self.weights.append(cnt)
+          result = 0
+          cnt = 0
+          res = []
+          for i in gu:
+            res.append(1 - hhi(np.array(self.partitioning)[g == i]))
+          self.stat.append(np.array(res))
+          self.statistic[c] = sum(self.stat[c] * self.weights[c])
+
+
+    def update_statistic(self, node: int, new_partition: int) -> None:
+        # delta: np.ndarray = np.zeros((1, self.spatial_lattice.L))
+        new_partitioning = self.partitioning.copy()
+        new_partitioning[node] = new_partition
+        for c in range(self.spatial_lattice.P):
+          aff_group = self.spatial_lattice.X[node,c]
+          g_part = np.array(new_partitioning)[np.array(self.spatial_lattice.X[:,c]) == aff_group]
+          self.stat[c][self.groups[c] == aff_group] = (1 - hhi(g_part))
+          self.statistic[c] = sum(self.stat[c] * self.weights[c])
+
+    def get_deltas(self, node: int, candidate_partitions: List[int]) -> List[np.ndarray]:
+        n_candidates: int = len(candidate_partitions)
+        deltas: List[np.ndarray] = [np.zeros((1, self.spatial_lattice.P)) for _ in range(n_candidates)]
+        new_partitioning = self.partitioning.copy()
+        for idx in range(n_candidates):
+          diff: np.ndarray = np.zeros((1, self.spatial_lattice.P))
+          new_partitioning[node] = candidate_partitions[idx]
+          for c in range(self.spatial_lattice.P):
+            aff_group = self.spatial_lattice.X[node,c]
+            g_part = np.array(new_partitioning)[self.spatial_lattice.X[:,c] == aff_group]
+            hhi_diff = (1 - hhi(g_part)) - self.stat[c][self.groups[c] == aff_group]
+            diff[c] =  hhi_diff * self.weights[c][self.groups[c] == aff_group]
+          deltas[idx] = diff
+          
+        return deltas
+
+## Packing - fast
+class VertexSuffocationNS(NetworkStatistic):
+    # NS that evaluates how suffocated groups are 
+    groups: List
+    weights: List
+    stat: List
+    
+    def __init__(self, spatial_lattice, partitioning, contributes_energy=True):
+        super().__init__(spatial_lattice, partitioning, contributes_energy)
+
+    def get_name(self) -> str:
+        return 'vertex_suffocation'
+      
+    def get_statistic_shape(self) -> Tuple[int, int]:
+        return self.spatial_lattice.P, 1
+
+    def compute_statistic(self) -> None:
+        self.statistic: np.ndarray = np.zeros((1, self.spatial_lattice.P))
+        gu, cnt = np.unique(self.partitioning, return_counts=True)
+        self.groups = gu
+        self.weights = cnt
+        self.stat: List(np.array) = []
+        for c in range(self.spatial_lattice.P):
+          x = np.array(self.spatial_lattice.X[:,c])
+          result = 0
+          cnt = 0
+          res = []
+          for i in gu:
+            res.append(1 - hhi(x[self.partitioning == i]))
+          self.stat.append(np.array(res))
+          self.statistic[c] = sum(self.stat[c] * self.weights[c])
+
+
+    def update_statistic(self, node: int, new_partition: int) -> None:
+        # delta: np.ndarray = np.zeros((1, self.spatial_lattice.L))
+        old_partition = self.partitioning[node]
+        new_partitioning = self.partitioning.copy()
+        new_partitioning[node] = new_partition
+
+        # Adjust old weight
+        old_idx = np.where(self.groups == old_partition)
+        self.weights[old_idx] -= 1
+
+        # Adjust new weight
+        if all(self.groups != new_partition):
+          self.groups= np.append(self.groups, new_partition)
+          self.weights = np.append(self.weights, int(0))
+          is_new = True
+        else:
+          is_new = False
+
+        new_idx = np.where(self.groups == new_partition)
+        self.weights[new_idx] += 1
+
+        for c in range(self.spatial_lattice.P):
+          if is_new:
+            self.stat[c]= np.append(self.stat[c], int(0))
+          
+          for p in [old_idx,new_idx]:
+            g_part = np.array(self.spatial_lattice.X[:,c])[np.array(new_partitioning) == self.groups[p]]
+            self.stat[c][p] = hhi(g_part)
+            
+          self.statistic[c] = sum(self.stat[c] * self.weights)
+            
+          if self.weights[old_idx] == 0:
+            self.stat[c] = np.delete(self.stat[c], old_idx)
+
+        if self.weights[old_idx] == 0:
+          self.weights = np.delete(self.weights, old_idx)
+          self.groups = np.delete(self.groups, old_idx)
+          
+          
+    def get_deltas(self, node: int, candidate_partitions: List[int]) -> List[np.ndarray]:
+        n_candidates: int = len(candidate_partitions)
+        deltas: List[np.ndarray] = [np.zeros((1, self.spatial_lattice.P)) for _ in range(n_candidates)]
+        new_partitioning = self.partitioning.copy()
+        old_partition = self.partitioning[node]
+        
+        # Adjust old weight
+        old_idx = np.where(self.groups == old_partition)
+        
+        for idx in range(n_candidates):
+          
+          diff: np.ndarray = np.zeros((1, self.spatial_lattice.P))
+          new_partitioning[node] = candidate_partitions[idx]
+          
+          temp_weights = self.weights.copy()
+          temp_weights[old_idx] -= 1
+          temp_groups = self.groups.copy()
+
+          # Adjust new weight
+          if all(temp_groups != candidate_partitions[idx]):
+            is_new = True
+            temp_weights = np.append(temp_weights, int(0))
+            temp_groups = np.append(temp_groups, candidate_partitions[idx])
+          else:
+            is_new = False
+          new_idx = np.where(temp_groups == candidate_partitions[idx])
+          temp_weights[new_idx] += 1
+          
+          for c in range(self.spatial_lattice.P):
+            temp_stat = self.stat[c].copy()
+            if is_new:
+              temp_stat = np.append(temp_stat, int(0))
+            
+            for p in [old_idx,new_idx]:
+              g_part = np.array(self.spatial_lattice.X[:,c])[np.array(new_partitioning) == temp_groups[p]]
+              temp_stat[p] = hhi(g_part)
+              
+            diff[c] = sum(temp_stat * temp_weights) - self.statistic
+          deltas[idx] = diff
+          
+        return deltas
+
+
+
 
 class PartitionModel:
     # Class for sampling and estimating partitionings on a SpatialLattice object
@@ -541,6 +718,14 @@ class PartitionModel:
         if "VertexPackNS" in net_stats:
           vertex_pack_ns: VertexPackNS = VertexPackNS(spatial_lattice, partitioning)
           self.network_statistics[vertex_pack_ns.get_name()] = vertex_pack_ns
+      
+        if "VertexDismembermentNS" in net_stats:
+          vertex_dismemberment_ns: VertexDismembermentNS = VertexDismembermentNS(spatial_lattice, partitioning)
+          self.network_statistics[vertex_dismemberment_ns.get_name()] = vertex_dismemberment_ns
+        
+        if "VertexSuffocationNS" in net_stats:
+          vertex_suffocation_ns: VertexSuffocationNS = VertexSuffocationNS(spatial_lattice, partitioning)
+          self.network_statistics[vertex_suffocation_ns.get_name()] = vertex_suffocation_ns
       
         # Set up force contiguous option
         if self.force_contiguous:
